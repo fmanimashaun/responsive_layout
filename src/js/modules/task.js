c8237003eb3c757f3a471adb4d916c4cd515ee5e
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-11-23 17:05:27 +0100

    Update setup

--- src/js/modules/task.js
+++
@@ -1,7 +0,0 @@
-export default class Task {
-  constructor() {
-    this.description = document.querySelector('.todos__label-input').value;
-    this.completed = false;
-    this.index = null;
-  }
-}
